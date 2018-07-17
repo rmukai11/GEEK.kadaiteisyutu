4f247fc170d1701e6c4a1dbaf7efbe904aff8f1d
amuro-chan.jp <r.mukai11@gmail.com>
2018-07-17 10:11:56 +0900

    メソッド3.4.5

---
+++ CampChallenge_java/CampChallenge_java/011.メソッド/Method3.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Camp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author amuro
+ */
+public class Method3 extends HttpServlet {
+
+    void multiplication(int i,int  j,boolean num,PrintWriter pw){
+        int result = (i * j);
+        if(num){
+            int goukei = (result * result);
+            pw.print(goukei);
+        }else{
+            int goukei = (result * 2);
+            pw.print(goukei);
+        }
+    }
+    
+    void multiplication(boolean num,PrintWriter pw){
+//        pw.print(result); 
+         multiplication(2,5,num,pw);
+    }
+    void multiplication(PrintWriter pw)  {   
+        multiplication(true,pw);
+    }
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+         
+            multiplication(2,2,false,out);
+            multiplication(true,out);
+        }
+       
+    }
+    
+       
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
